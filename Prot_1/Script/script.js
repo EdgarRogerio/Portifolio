0385199baf90c26a6cd3648e9e8704c638c8642b
edgarroger <edhot81@gmail.com>
2023-07-31 21:49:47 -0300

    commit

--- Prot_1/Script/script.js
+++ Prot_1/Script/script.js
@@ -1,3 +1,5 @@
+// Selecionar itens
+
 var menuItem = document.querySelectorAll('.item-menu')
 
 function select() 
@@ -11,3 +13,13 @@ function select()
 menuItem.forEach((item)=>
     item.addEventListener('click', select)
 )
+
+// Expandir itens 
+
+var sideExp = document.querySelector('#btn-exp')
+var sideMenu = document.querySelector('.nav-side')
+
+sideExp.addEventListener('click', function()
+{
+    sideMenu.classList.toggle('expand')
+})
